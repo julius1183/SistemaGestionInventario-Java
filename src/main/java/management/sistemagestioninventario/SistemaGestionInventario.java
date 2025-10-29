8366e9e276061b381971836f49ebba13ce54c5c6
julius1183 <juliocesarcordero_1983@hotmail.com>
2025-10-28 22:31:53 -0500

    Commit Inicial: Configuración del proyecto de Gestión de Productos.

---
+++ src/main/java/management/sistemagestioninventario/SistemaGestionInventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package management.sistemagestioninventario;
+
+/**
+ *
+ * @author Usuario
+ */
+public class SistemaGestionInventario {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al Sistema de Gestion de Inventario!");
+    }
+}
